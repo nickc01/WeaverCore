ae6f21d50eb36bb486a1382ffff648ab1cdc3c31
Nickc01 <nickc01@outlook.com>
2023-12-20 21:20:25 -0600

    Update DroppedItemBasic.cs

--- WeaverCore/Components/DroppedItemBasic.cs
+++ WeaverCore/Components/DroppedItemBasic.cs
@@ -1,18 +1,33 @@
-﻿using UnityEngine;
+using UnityEngine;
 using WeaverCore.Assets.Components;
 
 namespace WeaverCore.Components
 {
+    /// <summary>
+    /// Represents a basic dropped item in the game.
+    /// </summary>
     public class DroppedItemBasic : DroppedItem
     {
+        /// <summary>
+        /// Gets or sets the sprite of the item.
+        /// </summary>
         [field: SerializeField]
+        [Tooltip("The sprite of the item.")]
         Sprite ItemSprite { get; set; }
 
+        /// <summary>
+        /// The name of the item.
+        /// </summary>
         [field: SerializeField]
+        [Tooltip("The name of the item.")]
         string ItemName { get; set; }
 
+        /// <summary>
+        /// Called when the item is picked up by the player.
+        /// </summary>
         protected override void OnGiveItem()
         {
+            // Spawn the item get message with the specified sprite and name.
             ItemGetMessage.Spawn(ItemSprite, ItemName);
         }
     }

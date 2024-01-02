77219f8e65bda962820d26c073005c3c65422655
nickc01 <nickc01@outlook.com>
2024-01-02 17:52:50 -0600

    Fixed Shockwaves. Finished WeaverCompassUpdater

--- WeaverCore/Components/WeaverCompassUpdater.cs
+++ WeaverCore/Components/WeaverCompassUpdater.cs
@@ -1,35 +1,82 @@
 ﻿using UnityEngine;
+using WeaverCore.Attributes;
+using WeaverCore.Utilities;
 
 namespace WeaverCore.Components
 {
     public class WeaverCompassUpdater : MonoBehaviour
     {
-        [SerializeField]
-        bool updateOnAwake = true;
+        bool hooked = false;
 
         private void Awake()
         {
-            EventManager.OnEventTriggered += EventManager_OnEventTriggered;
-            /*if (updateOnAwake)
+            if (enabled)
             {
-                HeroController.instance.heroInPosition += Instance_heroInPosition;
-            }*/
+                OnEnable();
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (!hooked)
+            {
+                hooked = true;
+                EventManager.OnEventTriggered += EventManager_OnEventTriggered;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (hooked)
+            {
+                hooked = false;
+                EventManager.OnEventTriggered -= EventManager_OnEventTriggered;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            OnDisable();
         }
 
         private void EventManager_OnEventTriggered(string eventName, GameObject source, GameObject destination, EventManager.EventType eventType)
         {
-            //WeaverLog.Log("EVENT = " + eventName);
+            if (eventName == "SET COMPASS POINT" && destination == gameObject)
+            {
+                UpdateCompassPosition();
+            }
         }
 
-        /*private void Instance_heroInPosition(bool forceDirect)
-        {
-            UpdateCompassPosition();
-            HeroController.instance.heroInPosition -= Instance_heroInPosition;
-        }*/
+        /// <summary>
+        /// Updates the compass point on the map to whereever the player is currently located
+        /// </summary>
+        public void UpdateCompassPosition() => UpdateCompassPosition(Player.Player1.transform.position);
 
-        public void UpdateCompassPosition()
+        /// <summary>
+        /// Updates the compass point on the map to the specified position
+        /// </summary>
+        /// <param name="worldPosition">The position in the world the compass should point towards</param>
+        public void UpdateCompassPosition(Vector2 worldPosition)
         {
-            
+            var sceneName = GameManager.instance.GetSceneNameString();
+
+            var mapZone = GameManager.instance.GetCurrentMapZone();
+
+            var gameMapType = typeof(GameManager).Assembly.GetType("GameMap");
+
+            if (gameMapType != null)
+            {
+                var gameMap = GameObject.FindObjectOfType(gameMapType) as MonoBehaviour;
+
+                gameMap.ReflectCallMethod("SetDoorValues", new object[] { worldPosition.x, worldPosition.y, sceneName, mapZone });
+
+                PlayerData.instance.SetFloat("gMap_doorX", worldPosition.x);
+                PlayerData.instance.SetFloat("gMap_doorY", worldPosition.y);
+                PlayerData.instance.SetString("gMap_doorScene", sceneName);
+                PlayerData.instance.SetString("gMap_doorMapZone", mapZone);
+
+                gameMap.ReflectCallMethod("SetCompassPoint");
+            }
         }
     }
 }

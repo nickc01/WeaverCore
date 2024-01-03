23bb34de2743d20349ab97384a8fdd718ad9f634
nickc01 <nickc01@outlook.com>
2024-01-03 15:05:58 -0600

    Fixed issue in G_Enemy_I

--- Other Projects~/WeaverCore.Game/WeaverCore.Game/Implementations/G_Enemy_I.cs
+++ Other Projects~/WeaverCore.Game/WeaverCore.Game/Implementations/G_Enemy_I.cs
@@ -2,6 +2,7 @@
 using HutongGames.PlayMaker.Actions;
 using System.Collections;
 using UnityEngine;
+using WeaverCore.Assets.Components;
 using WeaverCore.Implementations;
 using WeaverCore.Utilities;
 

7227a4d9bb65bc24ced7506782f1f770cf3ddba2
nickc01 <nickc01@outlook.com>
2024-03-10 11:50:07 -0500

    Fixed crash when calling OnPlayerInit and OnPlayerUninit

--- WeaverCore/Player.cs
+++ WeaverCore/Player.cs
@@ -205,12 +205,24 @@ namespace WeaverCore
 			if (Player.Players.AddIfNotContained(this))
 			{
 				paramCache[0] = this;
-				//WeaverLog.Log("P_INIT_A");
-                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                //WeaverLog.Log("P_INIT_A");
+                /*foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
 				{
 					method.Invoke(null, paramCache);
-				}
-			}
+				}*/
+
+                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                {
+                    if (method.GetParameters().Length == 1)
+                    {
+                        method.Invoke(null, paramCache);
+                    }
+                    else
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
 		}
 
 		private void OnEnable()
@@ -219,9 +231,21 @@ namespace WeaverCore
 			{
                 paramCache[0] = this;
                 //WeaverLog.Log("P_INIT_B");
-                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                /*foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
                 {
                     method.Invoke(null, paramCache);
+                }*/
+
+                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerInitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                {
+                    if (method.GetParameters().Length == 1)
+                    {
+                        method.Invoke(null, paramCache);
+                    }
+                    else
+                    {
+                        method.Invoke(null, null);
+                    }
                 }
             }
 		}
@@ -232,9 +256,21 @@ namespace WeaverCore
 			{
                 paramCache[0] = this;
                 //WeaverLog.Log("P_INIT_C");
-                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                /*foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
                 {
                     method.Invoke(null, paramCache);
+                }*/
+
+                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                {
+                    if (method.GetParameters().Length == 1)
+                    {
+                        method.Invoke(null, paramCache);
+                    }
+                    else
+                    {
+                        method.Invoke(null, null);
+                    }
                 }
             }
 		}
@@ -245,9 +281,21 @@ namespace WeaverCore
 			{
                 paramCache[0] = this;
                 //WeaverLog.Log("P_INIT_D");
-                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                /*foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
                 {
                     method.Invoke(null, paramCache);
+                }*/
+
+                foreach (var (method, _) in ReflectionUtilities.GetMethodsWithAttribute<OnPlayerUninitAttribute>(Initialization.GetWeaverCoreAssemblies()))
+                {
+                    if (method.GetParameters().Length == 1)
+                    {
+                        method.Invoke(null, paramCache);
+                    }
+                    else
+                    {
+                        method.Invoke(null, null);
+                    }
                 }
             }
 		}

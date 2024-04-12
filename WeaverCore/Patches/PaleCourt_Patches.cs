c8ea8f4c97ed9bd20701dcf06751119ebb8fc8b4
nickc01 <nickc01@outlook.com>
2024-04-12 16:46:03 -0500

    Added patch to fix crash with Pale Court

---
+++ WeaverCore/Patches/PaleCourt_Patches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using WeaverCore;
+using WeaverCore.Attributes;
+
+public static class PaleCourt_Patches
+{
+    [OnHarmonyPatch]
+    static void OnHarmonyPatch(HarmonyPatcher patcher)
+    {
+        var paleCourtAsm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "PaleCourt");
+
+        if (paleCourtAsm != null)
+        {
+            WeaverLog.Log("FOUND PALE COURT");
+
+            var fiveKnightsType = paleCourtAsm.GetType("FiveKnights.FiveKnights");
+
+            var orig = fiveKnightsType.GetMethod("LoadPaleCourtMenuMusic", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var prefix = typeof(PaleCourt_Patches).GetMethod(nameof(Prefix), BindingFlags.NonPublic | BindingFlags.Static);
+
+            patcher.Patch(orig, prefix, null);
+        }
+    }
+
+    static bool Prefix(MusicCue musicCue)
+    {
+        return !(musicCue is WeaverMusicCue);
+    }
+}

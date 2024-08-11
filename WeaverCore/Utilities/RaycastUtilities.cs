5bfc5375a82f9bdf3bb9c0be0dacd73da415c3c4
Nickc01 <nickc01@outlook.com>
2024-08-11 01:07:03 -0500

    Added RaycastUtilities Class

---
+++ WeaverCore/Utilities/RaycastUtilities.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace WeaverCore.Utilities
+{
+    public static class RaycastUtilities
+    {
+        static RaycastHit2D[] hitCache = new RaycastHit2D[1];
+        static int terrainMask = -1;
+
+        public static int TerrainMask => terrainMask == -1 ? terrainMask = LayerMask.GetMask("Terrain") : terrainMask;
+
+        public static bool SimpleCastTo(Vector2 origin, Vector2 dest, out RaycastHit2D result, float distance = -1, int layerMask = -1)
+        {
+            if (distance < 0)
+            {
+                distance = (dest - origin).magnitude;
+            }
+            return SimpleCast(origin, (dest - origin).normalized, out result, distance, layerMask);
+        }
+
+        public static bool SimpleCast(Vector2 origin, Vector2 direction, out RaycastHit2D result, float distance, int layerMask = -1)
+        {
+            if (layerMask < 0)
+            {
+                layerMask = TerrainMask;
+            }
+            var count = Physics2D.RaycastNonAlloc(origin, direction, hitCache, distance, layerMask);
+
+            if (count > 0)
+            {
+                result = hitCache[0];
+            }
+            else
+            {
+                result = default;
+            }
+
+            return count > 0;
+        }
+    }
+}
